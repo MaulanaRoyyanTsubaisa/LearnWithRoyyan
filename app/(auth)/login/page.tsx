f120f8b272b5b665793e9aebc640fb66c2cf797a
Maulana Royyan Tsubaisa <maulanaroyyan33@gmail.com>
2025-07-17 16:37:09 +0700

    update auth github

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,44 +1,14 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { GithubIcon } from "lucide-react";
+import { headers } from "next/headers";
+import { LoginForm } from "./_components/LoginForm";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle className=" text-2xl font-">Welcome Back !</CardTitle>
-        <CardDescription>
-          Login With Your Github or Email Account
-        </CardDescription>
-      </CardHeader>
-
-      <CardContent className="flex flex-col gap-4">
-        <Button className="w-full" variant={"outline"}>
-          <GithubIcon className="size-4" />
-          Sign In with Github
-        </Button>
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-card px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-
-        <div className="grid gap-3">
-          <div className="grid gap-3">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" id="email" placeholder="example@example.com" />
-          </div>
-          <Button>Continue With Email</Button>
-        </div>
-      </CardContent>
-    </Card>
-  );
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    return redirect("/"); // Redirect to home if already logged in
+  }
+  return <LoginForm />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
-import Image from "next/image";
+import { auth } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
-  return <div className="text-2xl font-bold text-red-600">LearnWithRoyyan</div>;
-  <ThemeToggle />;
+  const router = useRouter();
+  // Use authClient to get the session
+  const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/"); // redirect to login page
+          toast.success("Successfully signed out!");
+        },
+      },
+    });
+  }
+  return (
+    <div className="p-24">
+      <h1 className="text-2xl font-bold text-red-600">LearnWithRoyyan</h1>
+      <ThemeToggle />
+      {session ? (
+        <div>
+          <p>{session.user.name} </p>
+          <Button onClick={signOut}>LogOut</Button>
+        </div>
+      ) : (
+        <Button>LogIn</Button>
+      )}
+    </div>
+  );
 }
